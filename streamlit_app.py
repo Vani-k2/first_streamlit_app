ef48b6f05d9c21e4eabf48a861682b95cd0d39a0
Vani-k2 <145990743+Vani-k2@users.noreply.github.com>
2023-09-25 18:36:11 +0530

    Update streamlit_app.py
    added breakfast menu

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,2 +1,7 @@
 import streamlit
 streamlit.title('My New Healthy Diner')
+streamlit.header('Breakfast Favorites')
+streamlit.text('🥣 Omega 3 & Blueberry Oatmeal')
+streamlit.text('🥗 Kale, Spinach & Rocket Smoothie')
+streamlit.text('🐔 Hard-Boiled Free_Range Egg')
+streamlit.text('🥑🍞 Avocado Toast')
